2dcb79d7c05ae4d8b47a58bd6f81d0df850e1b1b
StevenTannn <51817862+StevenTannn@users.noreply.github.com>
2019-06-14 18:46:27 +0700

    Update TugasJava.java

--- TugasJava.java
+++ TugasJava.java
@@ -3,7 +3,7 @@ public class TugasJava {
         System.out.println();
         Employee tania = new Employee("Tania", 100.0);
 
-        System.out.println("Nama : " + tania.getName() + "(" + tania.getClass().getName() + ")");
+        System.out.println("Nama : " + tania.getName() + " (" + tania.getClass().getName() + ") ");
         System.out.println("Gaji : " + tania.getSalary());
         System.out.println("Pajak : " + tania.getTax());
         System.out.println("Pendapatan Bersih : " + tania.getTakeHomePay());
@@ -12,12 +12,12 @@ public class TugasJava {
 
         ComissionedEmployee silvi = new ComissionedEmployee("Silvi", 100.0, 300.0);
 
-        System.out.println("Nama : " + silvi.getName() + "(" + silvi.getClass().getName() + ")");
+        System.out.println("Nama : " + silvi.getName() + " (" + silvi.getClass().getName() + ") ");
         System.out.println("Gaji : " + silvi.getSalary());
         System.out.println("Komisi : " + silvi.getComission());
         System.out.println("Pajak : " + silvi.getTax());
         System.out.println("Pendapatan Bersih : " + silvi.getTakeHomePay());
-        
+
         System.out.println();
     }
 }
